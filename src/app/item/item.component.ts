22660d444a70680997d31383c919f4c7e4f766e7
Mert1980 <mertdemirok80@gmail.com>
2021-12-02 14:14:57 +0100

    show all items

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Item} from "../model/Item";
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() item: Item | any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
